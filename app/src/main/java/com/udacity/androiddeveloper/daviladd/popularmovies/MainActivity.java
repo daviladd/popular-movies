b3ad15f6b86e8ffc90c493088a2d8296b88a7c6e
David Davila <fdaviddavila@gmail.com>
2018-05-24 14:53:16 +0200

    Adds basic functionality for a sorting order menu
    Adds a button to the main menu where the user can switch between two sorting order methods:
    + by popularity
    + by rating
    
    Just the click on these two entries are captured and a toast message is shown as a demonstrator.

--- app/src/main/java/com/udacity/androiddeveloper/daviladd/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/androiddeveloper/daviladd/popularmovies/MainActivity.java
@@ -2,6 +2,10 @@ package com.udacity.androiddeveloper.daviladd.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_sort_method, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_sort_method_popularity:
+                Toast.makeText(this,
+                        getText(R.string.debug_menu_sort_method_popularity),
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_sort_method_rating:
+                Toast.makeText(this,
+                        getText(R.string.debug_menu_sort_method_rating),
+                        Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
