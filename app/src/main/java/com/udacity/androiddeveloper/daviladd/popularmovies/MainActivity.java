ce085a0a5174776153c52b8d2da6a8c9feddc64e
David Davila <fdaviddavila@gmail.com>
2018-05-25 20:56:10 +0200

    Adds a basic grid view using RecyclerView

--- app/src/main/java/com/udacity/androiddeveloper/daviladd/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/androiddeveloper/daviladd/popularmovies/MainActivity.java
@@ -1,18 +1,55 @@
 package com.udacity.androiddeveloper.daviladd.popularmovies;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.udacity.androiddeveloper.daviladd.popularmovies.adapters.PopularMoviesAdapter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PopularMoviesAdapter mPopularMoviesAdapter;
+    private RecyclerView mRecyclerView;
+    private ProgressBar mLoadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the RecyclerView and the ProgressBar:
+        mRecyclerView = findViewById(R.id.recyclerview_movies);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        // Create a layout manager to handle the item views on the RecyclerView:
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+
+        //  Associate the layout manager with the RecyclerView:
+        mRecyclerView.setLayoutManager(layoutManager);
+        // TODO: check if this can be left this way:
+        mRecyclerView.setHasFixedSize(true);
+
+        mPopularMoviesAdapter = new PopularMoviesAdapter(this, createFakeMovies(20));
+        mRecyclerView.setAdapter(mPopularMoviesAdapter);
+
+        // TODO: show the progress bar while the data is being fetched and loaded
+        //loadindIndicatorShow();
+
     }
 
     @Override
@@ -27,11 +64,13 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.menu_sort_method_popularity:
+                // TODO: get the new data sorted by popularity
                 Toast.makeText(this,
                         getText(R.string.debug_menu_sort_method_popularity),
                         Toast.LENGTH_LONG).show();
                 return true;
             case R.id.menu_sort_method_rating:
+                // TODO: get the new data sorted by user rating
                 Toast.makeText(this,
                         getText(R.string.debug_menu_sort_method_rating),
                         Toast.LENGTH_LONG).show();
@@ -39,5 +78,35 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void loadindIndicatorShow() {
+        // Hide the movie items view:
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        // Show the loading indicator:
+        mLoadingIndicator.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hides the loading indicator in order to show the
+     */
+    private void loadindIndicatorHide() {
+        // Hide the loading indicator:
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        // Show the movie items view:
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+
+    List<PopularMovie> createFakeMovies(int quantity) {
+        List<PopularMovie> popularMovies = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            PopularMovie movie = new PopularMovie();
+            movie.setOriginalTitle("Movie " + i);
+            popularMovies.add(movie);
+        }
+
+        return popularMovies;
     }
 }
