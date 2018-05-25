b93199ddce5498832c3ff6a9215c9dd52989a9e5
David Davila <fdaviddavila@gmail.com>
2018-05-25 21:39:23 +0200

    Adds picasso and retrofit dependencies

---
+++ app/src/main/java/com/udacity/androiddeveloper/daviladd/popularmovies/adapters/PopularMoviesAdapter.java
@@ -0,0 +1,82 @@
+package com.udacity.androiddeveloper.daviladd.popularmovies.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.udacity.androiddeveloper.daviladd.popularmovies.PopularMovie;
+import com.udacity.androiddeveloper.daviladd.popularmovies.R;
+
+import java.util.List;
+
+public class PopularMoviesAdapter
+        extends RecyclerView.Adapter<PopularMoviesAdapter.PopularMoviesViewHolder> {
+    // Tag for logger:
+    private static final String TAG = PopularMoviesAdapter.class.getSimpleName();
+
+
+    private static final int VIEW_TYPE_POSTER_THUMBNAIL_GRID = 0;
+
+    private Context mContext;
+
+    private List<PopularMovie> mPopularMovies;
+
+    public PopularMoviesAdapter(@NonNull Context context, List<PopularMovie> popularMovies) {
+        mContext = context;
+        mPopularMovies = popularMovies;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mPopularMovies.size();
+    }
+
+    @Override
+    public PopularMoviesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layoutID;
+
+        switch (viewType) {
+            case VIEW_TYPE_POSTER_THUMBNAIL_GRID:
+                layoutID = R.layout.movie_thumbnail_item;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid view type, value of " + viewType);
+        }
+
+        View view = LayoutInflater.from(mContext).inflate(layoutID, parent, false);
+        view.setFocusable(true);
+
+        return new PopularMoviesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PopularMoviesViewHolder holder, int position) {
+        holder.movieOriginalTitle.setText(mPopularMovies.get(position).getOriginalTitle());
+        holder.moviePosterThumbnail.setImageResource(android.R.drawable.ic_menu_report_image);
+    }
+
+    public static class PopularMoviesViewHolder extends RecyclerView.ViewHolder
+            //        implements View.OnClickListener
+    {
+
+        final ImageView moviePosterThumbnail;
+        final TextView movieOriginalTitle;
+
+        PopularMoviesViewHolder(View view) {
+            super(view);
+
+            moviePosterThumbnail = view.findViewById(R.id.movie_poster_thumbnail_image);
+            movieOriginalTitle = view.findViewById(R.id.movie_poster_thumbnail_title);
+
+            //view.setOnClickListener(this);
+        }
+
+        // TODO: override onClick to handle the clicks on the View!
+    }
+}
